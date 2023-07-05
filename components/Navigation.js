92527adb3a7da288fb2bec6db5cc79761153710b
Saba <sabarht@gmail.com>
2023-07-05 22:17:22 +0200

    styling navbar

--- components/Navigation.js
+++ components/Navigation.js
@@ -1,11 +1,35 @@
 import Link from "next/link";
+import styled from "styled-components";
 
 export default function Navigation() {
   return (
-    <nav>
+    <StyledNavigation>
       <Link href="/">Spotlight</Link>
       <Link href="/art_pieces">Art pieces</Link>
       <Link href="/favorites">Favorites</Link>
-    </nav>
+    </StyledNavigation>
   );
 }
+const StyledNavigation = styled.nav`
+  position: sticky;
+  bottom: 0;
+  display: flex;
+  text-align: center;
+  justify-content: space-evenly;
+  align-items: center;
+
+  width: 100%;
+
+  a {
+    text-decoration: none;
+    padding: 1rem 2rem;
+    border: 1px solid black;
+    background-color: lavender;
+    font-weight: bold;
+    width: 100%;
+    &:hover {
+      color: white;
+      background-color: gold;
+    }
+  }
+`;
